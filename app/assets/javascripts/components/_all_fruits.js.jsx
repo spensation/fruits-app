7ce261aac2b79bb7b4c1df32bc45c4410c85a3bf
spensation <spensation@gmail.com>
2018-08-02 16:29:36 -0700

    able to read in this CRUD app

---
+++ app/assets/javascripts/components/_all_fruits.js.jsx
@@ -0,0 +1,33 @@
+class AllFruits extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			fruits: []
+		};
+	}
+
+	componentDidMount() {
+	console.log('hi')
+	fetch('/api/v1/fruits.json')
+		.then((response) => {return response.json()})
+		.then((data) => {this.setState({ fruits: data}) });
+	}
+
+	render() {
+		const fruits = this.state.fruits.map((fruit) => {
+			return(
+				<div key={fruit.id}>
+					<h1>{fruit.name}</h1>
+					<p>{fruit.description}</p>
+				</div>
+			)
+		})
+
+		return(
+			<div>
+				{fruits}
+			</div>
+		)
+	}
+}
